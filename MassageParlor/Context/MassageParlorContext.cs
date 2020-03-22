439a66bcf12902e6adec6ace5095cad0912b0bb2
Brandon Haskett <brhaskett@yahoo.com>
2020-03-22 18:02:44 -0400

    setting up entity logic and creating employee object

---
+++ MassageParlor/Context/MassageParlorContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MassageParlor.Context
+{
+    public class MassageParlorContext : DbContext
+    {
+        public MassageParlorContext(DbContextOptions<MassageParlorContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
